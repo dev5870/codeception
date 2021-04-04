ede9fe4afc3c7c8b9c09a6e40fa38cd85506568d
kostyashelest <skostya88@gmail.com>
2021-04-04 15:16:35 +0300

    Добавил данные для подключения БД

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=youtube_codeception',
+    'username' => 'youtube_codeception',
+    'password' => '12345',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
